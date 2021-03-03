3b58fb941732ef1f64d02225c2e64b9bd6b74815
심승용 <seungyong.shim@mirero.co.kr>
2021-03-04 08:40:17 +0900

    near_bombs_count 스팩

--- src/minesweeper/mineitem.py
+++ src/minesweeper/mineitem.py
@@ -1,5 +1,6 @@
 class MineItem:
     _is_bomb: bool
+    near_bombs_count: int
 
     def set_bomb(self):
         self._is_bomb = True
@@ -7,3 +8,6 @@ class MineItem:
     @property
     def is_bomb(self):
         return self._is_bomb
+
+    def __str__(self) -> str:
+        pass
--- tests/minesweeper/mineitem_tests.py
+++ tests/minesweeper/mineitem_tests.py
@@ -10,3 +10,8 @@ def test_is_bomb():
     sut = MineItem()
     sut.set_bomb()
     assert sut.is_bomb is True
+
+def test_near_bombs_count():
+    sut = MineItem()
+    sut.near_bombs_count = 7
+    assert str(sut) == "7"
