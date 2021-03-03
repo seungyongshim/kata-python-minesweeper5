c4b7db1cb6a51ed36e1f8f215e03ecf2676be41b
심승용 <seungyong.shim@mirero.co.kr>
2021-03-04 08:56:07 +0900

    MineItem 구현

--- src/minesweeper/mineitem.py
+++ src/minesweeper/mineitem.py
@@ -1,5 +1,6 @@
 class MineItem:
-    _is_bomb: bool
+    _is_bomb: bool = False
+    _is_cover: bool = True
     near_bombs_count: int
 
     def set_bomb(self):
@@ -9,5 +10,12 @@ class MineItem:
     def is_bomb(self):
         return self._is_bomb
 
-    def __str__(self) -> str:
+    def __str__(self):
+        if self._is_cover:
+            return "."
+        if self.is_bomb:
+            return "*"
         return str(self.near_bombs_count)
+
+    def click(self):
+        self._is_cover = False
--- tests/minesweeper/mineitem_tests.py
+++ tests/minesweeper/mineitem_tests.py
@@ -9,15 +9,19 @@ def test_instance():
 def test_is_bomb():
     sut = MineItem()
     sut.set_bomb()
+    sut.click()
     assert sut.is_bomb is True
+    assert str(sut) == "*"
 
 
 def test_near_bombs_count():
     sut = MineItem()
     sut.near_bombs_count = 7
+    sut.click()
     assert str(sut) == "7"
 
 
 def test_is_cover():
     sut = MineItem()
     assert str(sut) == "."
+
