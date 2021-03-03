8226d52600b12f73bc91dbba515e4cb68eab7cf0
심승용 <seungyong.shim@mirero.co.kr>
2021-03-04 08:41:40 +0900

    is_cover 스팩

--- tests/minesweeper/mineitem_tests.py
+++ tests/minesweeper/mineitem_tests.py
@@ -11,7 +11,13 @@ def test_is_bomb():
     sut.set_bomb()
     assert sut.is_bomb is True
 
+
 def test_near_bombs_count():
     sut = MineItem()
     sut.near_bombs_count = 7
     assert str(sut) == "7"
+
+
+def test_is_cover():
+    sut = MineItem()
+    assert str(sut) == "."
