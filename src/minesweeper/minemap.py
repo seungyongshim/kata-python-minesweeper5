ca940fc2c7255ae4cb1ce8f42974d6fc899f2e4e
심승용 <seungyongshim@nexon.co.kr>
2021-03-04 12:07:27 +0900

    set_bomb 구현

--- src/minesweeper/minemap.py
+++ src/minesweeper/minemap.py
@@ -1,8 +1,13 @@
+from typing import List, Tuple
 from minesweeper.mineitem import MineItem
+from toolz import pipe
 
 
 class MineMap:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, pos_bombs: List[Tuple[int, int]] = []):
         self._width = width
         self._height = height
         self._items = [[MineItem() for x in range(width)] for y in range(height)]
+
+        for item in map(lambda xy: self._items[xy[1]][xy[0]], pos_bombs):
+            item.set_bomb()
--- tests/minesweeper/minemap_tests.py
+++ tests/minesweeper/minemap_tests.py
@@ -2,9 +2,15 @@ from common.functools import flat_map
 from minesweeper.minemap import MineMap
 
 
-def test_instace():
+def test_instance():
     sut = MineMap(3, 4)
 
     assert sut._width is 3
     assert sut._height is 4
     assert len(list(flat_map(lambda x: x, sut._items))) is 12
+
+
+def test_set_bombs():
+    sut = MineMap(3, 4, [(0, 0), (1, 0)])
+    assert sut._items[0][0].is_bomb is True
+    assert sut._items[0][1].is_bomb is True
